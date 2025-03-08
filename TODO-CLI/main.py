e21573e2c23ccaa3a39dfc208b43d6adbe8d227b
billythegoat1 <nitungabilly@gmail.com>
2025-03-08 13:08:19 +0300

    Almost complete.  Still need revision.  Still have to write the code without looking somewhere else.  Still need to Learn about environments coz apparently it is better to work in them.

--- TODO-CLI/main.py
+++ TODO-CLI/main.py
@@ -1,5 +1,11 @@
 import click
 
+@click.group
+def mycommands():
+    pass
+
+
+
 PRIORITIES = {
     "o": "Optional",
     "l": "Low",
@@ -8,45 +14,63 @@ PRIORITIES = {
     "c": "Crucial"
 }
 
-#The first command the user will enter
-@click.command()
 
+@click.command()
 #The priority of the task. THIS MUST BE PROVIDED. IF NOT, IT WILL BE SET TO MEDIUM(M)
 @click.argument("priority", type=click.Choice(PRIORITIES.keys()), default="m")
-
 #THE PATH TO THE FILE IS NOT REQUIRED(required=0)
-@click.argument("todofile", type=click.Path(exists=False), required=0)
-
-#The user can choose or not to provide the name of the todo task
-@click.option("-n", "--name" prompt="Enter the todo name ", help="The name of the todo task")
-
-#The user can choose or not to provid a description for the todo task
+@click.argument("todofile", type=click.Path(exists=False), default="mytodos.txt", required=False)
+#The user is prompted to enter the name of the task
+@click.option("-n", "--name", prompt="Enter the todo name", help="The name of the todo task")
+#The user is prompted to enter a description for the task
 @click.option("-d", "--description", prompt="Describe the todo task", help="The description of the todo task")
-
 #Function to add a task
-def add_todo(name, description, priority, todofile):
+def add_task(name, description, priority, todofile):
     
-    filename = todofile if todofile is not None else 'mytodos.txt'
     #a+ : Append to the file. If it doesn't exist we create it.
-    with open(filename, "a+") as f:
-        f.write(f"{name}: {description} [Priority: {PRIORITIES}]")
+    with open(todofile, "a+") as f:
+        f.write(f"{name}: {description} [Priority: {PRIORITIES[priority]}]\n")
         
 
+#Display the tasks
+@click.command()
+@click.option("-p", "--priority", type=click.Choice(PRIORITIES.keys()), required=0)
+@click.argument("todofile", type=click.Path(exists=True), default="mytodos.txt", required=0)
+def list_task(priority, todofile):
+    with open(todofile, "r") as f:
+        
+        todo_list = f.read().splitlines()
+        if priority is None:
+            for idx, task in enumerate(todo_list, start=1):
+                print(f"({idx}) - {task}")
+        else:
+            for idx, task in enumerate(todo_list, start=1):
+                if f"[Priority = {PRIORITIES[priority]}]" in task:
+                    print(f"{idx} - {task}")
+                        
+#DELETE A TASK
 @click.command() 
-@click.argument("idx", type=int, required=1)
+@click.argument("idx", type=int, required=True)
 #Function to delete a todo task
-def delete(idx):
+def delete_task(idx):
     #r: reading mode
     with open("mytodos.txt", "r") as f:
-        #We want to put the file in a variable before altering it
+        #We want sepatate each task on a single line
         todo_list = f.read().splitlines()
-        todo_list.pop(idx)
+        #Then delete the one we want to delete
+        todo_list.pop(idx-1)
         
+        #Rewrite the remaining tasks in the file
     with open("mytodos.txt", "w") as f: 
         f.write("\n".join(todo_list))
         f.write('\n')
 
+mycommands.add_command(add_task)
+mycommands.add_command(list_task)
+mycommands.add_command(delete_task)
 
 if __name__ == "__main__":
-    hello() 
+    
+    mycommands()
+                    
     
